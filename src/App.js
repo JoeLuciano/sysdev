40a7dfcfa8e0bb4dfd336bf08d3023ecfa21d6ad
JoeLuciano <joeluciano@outlook.com>
2022-05-27 17:41:08 -0400

    Added xarrows

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { useState, useEffect } from 'react';
 import { motion, useViewportScroll } from 'framer-motion';
 import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
+import Xarrow from 'react-xarrows';
 
 const Learning = () => {
   const [isZoomedIn, setIsZoomedIn] = useState(false);
@@ -28,6 +29,8 @@ const Learning = () => {
                     padding: '50px',
                     minHeight: '300px',
                     width: '100%',
+                    display: 'flex',
+                    gridGap: '2rem',
                   }}>
                   <div
                     id='element1'
@@ -37,6 +40,18 @@ const Learning = () => {
                       height: '400px',
                     }}>
                     Zoom element 1
+                  </div>
+                  <Xarrow start='element1' end='element2'>
+                    Test
+                  </Xarrow>
+                  <div
+                    id='element2'
+                    style={{
+                      background: 'orange',
+                      width: '200px',
+                      height: '200px',
+                    }}>
+                    Zoom element 2
                   </div>
                 </div>
               ) : (
@@ -49,13 +64,13 @@ const Learning = () => {
                     width: '100%',
                   }}>
                   <div
-                    id='element2'
+                    id='element3'
                     style={{
                       background: 'blue',
                       width: '200px',
                       height: '200px',
                     }}>
-                    Zoom element 2
+                    Zoom element 3
                   </div>
                 </div>
               )}
