22da930c216b971e8769351280c07ef0a0373130
JoeLuciano <joeluciano@outlook.com>
2022-05-27 15:02:04 -0400

    Initial testing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { motion, useViewportScroll } from 'framer-motion';
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
+
+const Learning = () => {
+  const [isZoomedIn, setIsZoomedIn] = useState(false);
+  return (
+    <TransformWrapper>
+      {(args) => {
+        if (args.state.scale > 1) {
+          setIsZoomedIn(true);
+        } else {
+          setIsZoomedIn(false);
+        }
+
+        return (
+          <>
+            <div>
+              <button onClick={() => args.resetTransform()}>Reset</button>
+            </div>
+            <TransformComponent>
+              {isZoomedIn ? (
+                <div
+                  style={{
+                    background: '#004',
+                    color: 'white',
+                    padding: '50px',
+                    minHeight: '300px',
+                    width: '100%',
+                  }}>
+                  <div
+                    id='element1'
+                    style={{
+                      background: 'red',
+                      width: '200px',
+                      height: '400px',
+                    }}>
+                    Zoom element 1
+                  </div>
+                </div>
+              ) : (
+                <div
+                  style={{
+                    background: '#444',
+                    color: 'white',
+                    padding: '50px',
+                    minHeight: '300px',
+                    width: '100%',
+                  }}>
+                  <div
+                    id='element2'
+                    style={{
+                      background: 'blue',
+                      width: '200px',
+                      height: '200px',
+                    }}>
+                    Zoom element 2
+                  </div>
+                </div>
+              )}
+            </TransformComponent>
+          </>
+        );
+      }}
+    </TransformWrapper>
+  );
+};
 
 function App() {
+  const scroll = useViewportScroll();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Learning></Learning>
     </div>
   );
 }
